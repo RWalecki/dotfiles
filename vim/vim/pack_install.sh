d334664c8c2b8b11e97798af0819f4c1292403c2
Robert Walecki <rw2614@imperial.ac.uk>
2017-11-28 02:26:52 +0000

    added different vim packages

--- vim/vim/pack_install.sh
+++ vim/vim/pack_install.sh
@@ -4,6 +4,9 @@ git submodule add https://github.com/scrooloose/nerdcommenter.git         pack/e
 git submodule add https://github.com/scrooloose/nerdtree.git              pack/editor/start/nerdtree.git
 git submodule add https://github.com/vim-airline/vim-airline.git          pack/editor/start/vim-airline.git
 git submodule add https://github.com/jistr/vim-nerdtree-tabs.git          pack/editor/start/vim-nerdtree-tabs.git
+git submodule add https://github.com/junegunn/vim-easy-align.git          pack/editor/start/vim-easy-align.git
+git submodule add https://github.com/airblade/vim-gitgutter.git           pack/editor/start/vim-gitgutter.git
+git submodule add https://github.com/tpope/vim-fugitive.git               pack/editor/start/vim-fugitive.git
 
 git submodule add https://github.com/Addisonbean/Vim-Xcode-Theme.git      pack/themes/start/Vim-Xcode-Theme.git
 git submodule add https://github.com/tomasr/molokai.git                   pack/themes/start/molokai.git
