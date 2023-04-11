25d55e5a0a63fcdaa9b1e1ead1ec3bf0554d4f49
Robert Walecki <rw2614@imperial.ac.uk>
2023-04-11 16:27:39 +0000

    Refactor installation script to use absolute paths and added comments

--- install.sh
+++ install.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
-# backup dotfiles
-mkdir -p $HOME/.dotfiles_backup
+# Get the directory where the script file is located
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-# create symlinks 
-for rcfile in $PWD/*/*; do
-        filename=$(basename "$rcfile")
-        mv -f $HOME/.$filename $HOME/.dotfiles_backup 2>/dev/null
-        ln -sf $rcfile $HOME/.$filename
+# Loop through all files and directories in the script directory
+for rcfile in $SCRIPT_DIR/*/*; do
+  # Get the filename of the current file
+  filename=$(basename "$rcfile")
+  
+  # Remove any existing configuration file with the same name in the user's home directory
+  rm -r $HOME/.$filename
+  
+  # Create a symbolic link to the file found in the user's home directory
+  ln -sf $rcfile $HOME/.$filename
 done
 
-# clone zprezto 
+# Remove any existing zprezto configuration in the user's home directory
 rm -rf ~/.zprezto
+
+# Clone the zprezto repository from GitHub into the user's home directory
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-# pull all submodules (vim packages)
+# Update all submodules (vim packages) in the zprezto repository
 git submodule update --init --recursive
